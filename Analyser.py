819150219d2ff7ba934d166f6267dd3927ba8469
BenedictSu <Benedict_S_S@u.nus.edu>
2015-10-13 03:03:19 +0800

    simple naive matching

--- Analyser.py
+++ Analyser.py
@@ -1,19 +1,28 @@
 # reference to http://www.nltk.org/book/ch07.html
-import nltk, re, pprint
+import nltk, re, pprint, sets
 
 from Parser import convert_pdf_to_file
+from nltk.corpus import treebank_chunk
 
+pdfFilePath = raw_input("Please enter the location of the JD in pdf format: ")
+jd = convert_pdf_to_file(pdfFilePath).decode('utf-8')
 pdfFilePath = raw_input("Please enter the location of the CV in pdf format: ")
-
-convertedFile = convert_pdf_to_file(pdfFilePath).decode('utf-8')
+cv = convert_pdf_to_file(pdfFilePath).decode('utf-8')
 
 def processing (cFile):
-    tokens = nltk.word_tokenize(cFile)
+    tokens = nltk.wordpunct_tokenize(cFile)
+    tset = sets.Set(tokens)
+    return tset
+    
+cv = processing(cv)
+jd = processing(jd)
 
-    taggedTokens = nltk.pos_tag(tokens)
+total = 0
+matched = 0
+for key in cv:
+    if key in jd:
+        matched += 1
+    total += 1
 
-    chunked = nltk.chunk.ne_chunk(taggedTokens, False)
-
-    print chunked
-
-processing(convertedFile)
+print "The percentage matched is: "
+print float(matched) / total
